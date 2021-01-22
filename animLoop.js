4ba396342db2b7a4dddaadc721870560bbf76a2a
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-22 11:56:58 +0100

    Update animLoop.js

--- animLoop.js
+++ animLoop.js
@@ -3,21 +3,22 @@ var context = canvas.getContext('2d');
 var width = canvas.width = 400;
 var height = canvas.height = 400;
 
-var zOff = 0;
+var xOff = 0;
 
 var loop = true;
 var requestAnimId = 0;
 var targetFrameRate = 10;
 var lastFrameTime = window.performance.now();
 
+
 function draw() {
   var now = window.performance.now();
-  var time_since_last = now - lastFrameTime;
-  var target_time_between_frames = 1000.0 / targetFrameRate;
+  var then = now - lastFrameTime;
+  var interval = 1000.0 / targetFrameRate;
   var epsilon = 5;
 
   if (!loop ||
-    time_since_last >= target_time_between_frames - epsilon) {
+    then >= interval - epsilon) {
 
     redraw();
 
@@ -27,13 +28,12 @@ function draw() {
   }
   if (loop) {
     requestAnimId = window.requestAnimationFrame(draw);
-
   }
 }
 
 function redraw() {
-  context.fillRect(zOff,0,5,5);
-  zOff += 5;
+  context.fillRect(xOff,0,5,5);
+  xOff += 5;
 }
 
 draw();
