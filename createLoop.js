dc6c223be10bf66f3170be976fee4c15821e7f5d
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-23 17:06:57 +0100

    Create createLoop.js

---
+++ createLoop.js
@@ -0,0 +1,72 @@
+import now from "right-now";
+import EventEmitter from "events";
+import raf from "raf";
+
+/*
+* Set to 30 frame/s
+*
+*let count = 0;
+*var loop = new Loop(function (dt) {
+*  console.log(dt);
+*  count++;
+*  if (count >= 10) {
+*    loop.stop();
+*  }
+*}, 30).start();
+
+
+class Loop {
+  constructor(callBack, frameRate) {
+    this.nEvent = new EventEmitter();
+
+    if (!(this instanceof Loop)) {
+      return new Loop(callBack);
+    }
+
+    this._loop = true;
+    this._requestAnimId = 0;
+    this._targetFrameRate = frameRate || 60;
+    this._lastFrameTime = now();
+    this._tick = this.tick.bind(this);
+
+    if (callBack) {
+      this.nEvent.on("tick", callBack);
+    }
+  }
+
+  start() {
+    if (this._loop) {
+      this._requestAnimId = raf(this._tick);
+    }
+    return this;
+  }
+
+  stop() {
+    this._loop = false;
+    if (this._requestAnimId) {
+      raf.cancel(this._requestAnimId);
+    }
+    this._requestAnimId = 0;
+    return this;
+  }
+
+  tick() {
+    const rightNow = now();
+    const then = rightNow - this._lastFrameTime;
+    const frameInterval = 1000.0 / this._targetFrameRate;
+    const epsilon = 5;
+
+    if (!this._loop || then >= frameInterval - epsilon) {
+      //   this._frameRate = 1000.0 / (rightNow - this._lastFrameTime);
+      this.deltaTime = rightNow - this._lastFrameTime;
+      this.nEvent.emit("tick", this.deltaTime);
+      this._lastFrameTime = rightNow;
+    }
+
+    if (this._loop) {
+      this._requestAnimId = raf(this._tick);
+    }
+  }
+}
+
+export default Loop;
